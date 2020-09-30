e468935b90285885ac83ec13e1413c44359d9667
lingqiz <zlqzcc@gmail.com>
2020-09-29 23:44:24 -0400

    minor update

--- tutorials/t_temporalUniformThreshold.m
+++ tutorials/t_temporalUniformThreshold.m
@@ -38,29 +38,27 @@ theClassifierEngine = responseClassifierEngine(@pcaSVMClassifier);
 nullContrast = 0.0;
 [theNullSceneSequence, theSceneTemporalSupportSeconds] = theSceneEngine.compute(nullContrast);
 
-
 % Construct a QUEST threshold estimator
 % estimate threshold on log contrast
 estDomain  = -3.5 : 0.01 : -1;
 slopeRange = 1.0 : 1.0 : 200;
 
-
 % Run QUEST for a total of minTrial trials. For typical usage, this is recommended
 
 % However, when numEstimator > 1, an adpative procedure is invoked,
-% for which the routine will stop if the S.E. among N parallel quest+ object 
+% for which the routine will stop if the S.E. among N parallel quest+ object
 % is below the threshold specified as stopCriterion
-estimator = QuestThresholdEngine('minTrial', 1280, 'maxTrial', 1280, ...
+estimator = QuestThresholdEngine('minTrial', 900, 'maxTrial', 6000, ...
     'estDomain', estDomain, 'slopeRange', slopeRange, ...
-    'numEstimator', 1, 'stopCriterion', 0.05);
+    'numEstimator', 3, 'stopCriterion', 0.05);
 
 %% Threshold estimation with QUEST+
 
 [logContrast, nextFlag] = estimator.nextStimulus();
 
-% 128 trial for each contrast level
+% 150 trial for each contrast level
 % 512 trials for training and testing
-nRepeat = 128;
+nRepeat = 150;
 nTrainSample = 512;
 while (nextFlag)
     
@@ -68,8 +66,6 @@ while (nextFlag)
     % Compute the TEST stimulus
     testContrast = 10 ^ logContrast;
     [theTestSceneSequence, ~] = theSceneEngine.compute(testContrast);
-    
-    fprintf('Current test contrast: %.4f ', testContrast);
     
     % Code for observer (classifier or other form)
     % Compute many respose instances to the NULL and TEST stimuli for training the SVM classifier
@@ -94,16 +90,19 @@ while (nextFlag)
         inSampleTestStimResponses('random'));
     
     pCorrect = trainingData.pCorrectInSample;
-    fprintf('P-correct: %.3f \n', pCorrect);
     
     % Translate p-correct into binary response using binomial distribution
     % Not necessary for template-based observer
     nCorrect = binornd(nRepeat, pCorrect);
     response = [zeros(1, nRepeat - nCorrect), ones(1, nCorrect)];
     
+    fprintf('Current test contrast: %.4f, P-correct: %.4f \n', testContrast, pCorrect);
+    
     [logContrast, nextFlag] = ...
         estimator.multiTrial(logContrast * ones(1, nRepeat), response(randperm(length(response))));
     
+    [threshold, stderr] = estimator.thresholdEstimate();
+    fprintf('Current threshold estimate: %.4f, stderr: %.4f \n', threshold, stderr);
 end
 
 %% Show results
@@ -111,13 +110,13 @@ fprintf('%d trials recorded \n', estimator.nTrial);
 
 % Plot results
 figure(); subplot(1, 2, 1);
-[threshold, para] = estimator.thresholdMLE('showPlot', true, 'pointSize', 10);
+[threshold, para] = estimator.thresholdMLE('showPlot', true, 'pointSize', 7.5);
 
 fprintf('Maximum likelihood fit parameters: %0.2f, %0.2f, %0.2f, %0.2f\n', ...
     para(1), para(2), para(3), para(4));
 
 %% Validation by computing the whole psychometric curve
-logContrast = -3.5 : 0.125 : -1;
+logContrast = -3 : 0.05 : -1.5;
 pCorrect = zeros(1, length(logContrast));
 
 nTrainSample = 512;
@@ -149,4 +148,5 @@ end
 
 subplot(1, 2, 2);
 plot(logContrast, pCorrect, '-ok', 'LineWidth', 2);
+xlim([-3.5, -1]);
 ylim([0, 1]);
