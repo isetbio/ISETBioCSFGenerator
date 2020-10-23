9892b4a6a72da85d7a0fbb58ec59be67d4621298
David Brainard <brainard@psych.upenn.edu>
2020-10-23 10:11:08 -0400

    Call out to separate ComputePerformanceTAFC

--- tutorials/t_thresholdEngine.m
+++ tutorials/t_thresholdEngine.m
@@ -22,7 +22,7 @@
 %
 % See Also:
 %   t_sceneGeneration, t_modulatedGratingsSceneGeneration,
-%   t_neuralResponseCompute, t_responseClassifier
+%   t_neuralResponseCompute, t_responseClassifier, computePerformanceTAFC
 %
 % History:
 %   10/05/20  dhb   Add logic to cache trained classifiers so we don't
@@ -378,14 +378,17 @@ while (nextFlag)
         % Train classifier for this TEST contrast and get predicted
         % correct/incorrect predictions.  This function also computes the
         % neural responses needed to train and predict.
-        [predictions, theTrainedClassifierEngines{testedIndex}] = computePerformance(...
+        %
+        % See helper function computePerformanceTAFC for how we use the
+        % components to get trial by trial predictions.
+        [predictions, theTrainedClassifierEngines{testedIndex}] = computePerformanceTAFC(...
             theNullSceneSequence, theTestSceneSequences{testedIndex}, ...
             theSceneTemporalSupportSeconds, nTrain, nTest, ...
             theNeuralEngine, theRawClassifierEngine, trainFlag, testFlag);
         
     else
         % Classifier is already trained, just get predictions
-        predictions = computePerformance(...
+        predictions = computePerformanceTAFC(...
             theNullSceneSequence, theTestSceneSequences{testedIndex}, ...
             theSceneTemporalSupportSeconds, nTrain, nTest, ...
             theNeuralEngine, theTrainedClassifierEngines{testedIndex}, [], testFlag);
@@ -428,7 +431,7 @@ if (runValidation)
         testContrast = 10 ^ logContrast(idx);
         [theTestSceneSequence, ~] = theSceneEngine.compute(testContrast);
         
-        pCorrect(idx) = mean(computePerformance(...
+        pCorrect(idx) = mean(computePerformanceTAFC(...
             theNullSceneSequence, theTestSceneSequence, ...
             theSceneTemporalSupportSeconds, 512, 512, ...
             theNeuralEngine, theRawClassifierEngine), trainFlag, testFlag);
@@ -440,71 +443,3 @@ if (runValidation)
     
 end
 
-%% Helper function
-function [predictions, theClassifierEngine] = computePerformance(nullScene, testScene, temporalSupport, nTrain, nTest, theNeuralEngine, theClassifierEngine, trainFlag, testFlag)
-
-% Train the classifier.
-%
-% If trainFlag is empty, then the passed classifier has already been trained
-% and training is skipped.  Otherwise trainFlag is passed to the stimulus
-% generation routine to indicate what type of noise (typically 'none' or
-% 'random') should be used in the training.
-if (~isempty(trainFlag))
-    % Generate stimulus for training, NULL stimulus
-    [inSampleNullStimResponses, ~] = theNeuralEngine.compute(...
-        nullScene, ...
-        temporalSupport, ...
-        nTrain, ...
-        'noiseFlags', {trainFlag});
-    
-    % Generate stimulus for training, TEST stimulus
-    [inSampleTestStimResponses, ~] = theNeuralEngine.compute(...
-        testScene, ...
-        temporalSupport, ...
-        nTrain, ...
-        'noiseFlags', {trainFlag});
-    
-    % Train the classifier. This shows the usage to extact information
-    % from the container retrned as the first return value from the neural
-    % response engine - we index the responses by the string contained in
-    % the variable trainFlag (which was itself passed to the neural
-    % repsonse engine above.)
-    %
-    % Once extracted from the container, the responses are a 3 dimensional
-    % matrix, with the dimensions indexing [instancesNum x mNeuralDim x tTimeBins].
-    %   instancesNum   - number of response instances
-    %   mNeuralDim     - dimension of neural response at one timepoint
-    %   tTimeBins      - number of time points in stimulus sequence.
-    theClassifierEngine.compute('train', ...
-        inSampleNullStimResponses(trainFlag), ...
-        inSampleTestStimResponses(trainFlag));
-end
-
-% Predict using trained classifier.
-%
-% Generate stimulus for prediction, NULL stimulus.  The variable testFlag
-% indicates what type of noise is used to generate the stimuli used for
-% prediction.  Typically 'random'.
-[inSampleNullStimResponses, ~] = theNeuralEngine.compute(...
-    nullScene, ...
-    temporalSupport, ...
-    nTest, ...
-    'noiseFlags', {testFlag});
-
-% Generate stimuli for prediction, TEST stimulus
-[inSampleTestStimResponses, ~] = theNeuralEngine.compute(...
-    testScene, ...
-    temporalSupport, ...
-    nTest, ...
-    'noiseFlags', {testFlag});
-
-% Do the prediction
-dataOut = theClassifierEngine.compute('predict', ...
-    inSampleNullStimResponses(testFlag), ...
-    inSampleTestStimResponses(testFlag));
-
-% Set return variable.  For each trial 0 means wrong and 1 means right.
-% Taking mean(response) gives fraction correct.
-predictions = dataOut.trialPredictions;
-
-end
