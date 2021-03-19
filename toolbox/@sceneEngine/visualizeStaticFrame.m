ca82cfc9ad82cbbfed439d9d0d4448f9f1145843
Nicolas Cottaris <nicolas@santorini.myfiosgateway.com>
2021-03-19 19:13:39 -0400

    Now printing a warning when stimulus pixels are out of gamut.

--- toolbox/@sceneEngine/visualizeStaticFrame.m
+++ toolbox/@sceneEngine/visualizeStaticFrame.m
@@ -33,10 +33,36 @@ for frameIndex = 1:scenesNum
         y = y-mean(y);
     end
     
-    % Linear RGB image
-    displayLinearRGBimage = imageLinearTransform(xyzImage, displayXYZToLinearRGB);
-    % Settings RGB image
-    displaySettingsImage = (ieLUTLinear(displayLinearRGBimage, displayGet(presentationDisplay, 'inverse gamma'))) / displayGet(presentationDisplay, 'nLevels');
+    debugMode = ~true;
+    if (debugMode)
+        displaySettingsImage = sceneGet(sceneSequence{frameIndex}, 'rgb');
+    else
+        % Linear RGB image
+        displayLinearRGBimage = imageLinearTransform(xyzImage, displayXYZToLinearRGB);
+        
+        % Find out-of-gamut pixels
+        pixelsNum = numel(displayLinearRGBimage);
+        belowZeroIdx = find(displayLinearRGBimage<0);
+        overOneIdx = find(displayLinearRGBimage>1);
+        
+        % Deal with out-of-gamut pixels
+        displayLinearRGBimage(belowZeroIdx) = 0;
+        displayLinearRGBimage(overOneIdx) = 1;
+        
+        % Inform the user about the out-of-gamut pixels
+        if ((numel(belowZeroIdx)>0) || (numel(overOneIdx)>0))
+             fprintf(2,'Warning: out-of-gamut pixels in frame %d.\n', frameIndex);
+        end
+        if (numel(belowZeroIdx)>0)
+            fprintf(2,'\t%2.1f of the image pixels  were < 0.\n', 100*numel(belowZeroIdx)/pixelsNum);
+        end
+        if (numel(overOneIdx)>0)
+            fprintf(2,'\t%2.1f of the image pixels were > 1)\n', 100*numel(overOneIdx)/pixelsNum);
+        end
+        
+        % Settings RGB image
+        displaySettingsImage = (ieLUTLinear(displayLinearRGBimage, displayGet(presentationDisplay, 'inverse gamma'))) / displayGet(presentationDisplay, 'nLevels');
+    end
     
     % Render image
     image(x,y,displaySettingsImage);
