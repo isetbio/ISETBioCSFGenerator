c4c99b2786fd75fb3b4c6b122a9020296932b6ad
FangfangHong <fh862@nyu.edu>
2023-09-28 19:05:09 -0400

    After 09/28 meeting

--- computeFunctions/neuralResponse/nrePhotopigmentExcitationsCmosaicWithNoEyeMovements.m
+++ computeFunctions/neuralResponse/nrePhotopigmentExcitationsCmosaicWithNoEyeMovements.m
@@ -126,144 +126,159 @@ function dataOut = nrePhotopigmentExcitationsCmosaicWithNoEyeMovements(...
     randomNoiseResponseInstances = theResponses('random');
 %}
 
-    % Check input arguments. If called with zero input arguments, just return the default params struct
-    if (nargin == 0)
-        dataOut = generateDefaultParams();
-        return;
-    end
-    
-    % Parse the input arguments
-    p = inputParser;
-    p.addParameter('noiseFlags', {'random'});
-    p.addParameter('rngSeed',[],@(x) (isempty(x) | isnumeric(x)));
-    varargin = ieParamFormat(varargin);
-    p.parse(varargin{:});
-    
-    % Retrieve the response noiseFlag labels and validate them.
-    noiseFlags = p.Results.noiseFlags;
-    rngSeed = p.Results.rngSeed;
-    neuralEngineOBJ.validateNoiseFlags(noiseFlags);
-    
-    % For each noise flag we generate a corresponing neural response, and all 
-    % neural responses are stored in a dictionary indexed by the noiseFlag label.
-    % Setup theNeuralResponses dictionary, loading empty responses for now
-    theNeuralResponses = containers.Map();
-    for idx = 1:length(noiseFlags)
-        theNeuralResponses(noiseFlags{idx}) = [];
-    end
-    
-    if (isempty(neuralEngineOBJ.neuralPipeline))
-        % Generate the optics
-%         theOptics = oiCreate(neuralResponseParamsStruct.opticsParams.type, neuralResponseParamsStruct.opticsParams.pupilDiameterMM);  
+    boolean_callingMasterFunc = input(sprintf(['Warning: this function has been deprecated and ',...
+        'replaced by nrePhotopigmentExcitationsCmosaic.m.\nIf you''d like to ',...
+        'switch to the new function, type ''yes'', otherwise, type ''no'':']), ...
+        's');
 
-        % Generate the cone mosaic
-        % Modified by Mengxin 2023/03/14: switched from using coneMosaicHex
-        % to cMosaic; eccentricity added
-        theConeMosaic = cMosaic(...
-            'sizeDegs', neuralResponseParamsStruct.coneMosaicParams.sizeDegs, ... 
-            'eccentricityDegs', neuralResponseParamsStruct.coneMosaicParams.eccDegs, ...
-            'integrationTime', neuralResponseParamsStruct.coneMosaicParams.timeIntegrationSeconds ...
-            );
-        
-        % Generate optics appropriate for the mosaic's eccentricity
-        oiEnsemble = theConeMosaic.oiEnsembleGenerate(neuralResponseParamsStruct.coneMosaicParams.eccDegs, ...
-            'zernikeDataBase', 'Polans2015', ...
-            'subjectID', neuralResponseParamsStruct.opticsParams.PolansSubject, ...
-            'pupilDiameterMM', neuralResponseParamsStruct.opticsParams.pupilDiameterMM);
-        theOptics = oiEnsemble{1};
-        returnTheNeuralPipeline = true;
-    else
-        % Load the optics from the previously computed neural pipeline
-        theOptics = neuralEngineOBJ.neuralPipeline.optics;
-        % Load the cone mosaic from the previously computed neural pipeline
-        theConeMosaic = neuralEngineOBJ.neuralPipeline.coneMosaic;
-        returnTheNeuralPipeline =  false;
-    end
+    %call the new master function
+    if strcmp(boolean_callingMasterFunc,'yes')
+        %before we do that, we need to do some sanity checks
+        %1. check if all oisequence 
+    %do it old way
+    elseif strcmp(boolean_callingMasterFunc,'no')
 
-    % Compute the sequence of optical images corresponding to the sequence of scenes
-    framesNum = numel(sceneSequence);
-    theListOfOpticalImages = cell(1, framesNum);
-    for frame = 1:framesNum
-        theListOfOpticalImages{frame} = oiCompute(sceneSequence{frame}, theOptics);
-    end
-    
-    % Generate an @oiSequence object containing the list of computed optical images
-    theOIsequence = oiArbitrarySequence(theListOfOpticalImages, sceneSequenceTemporalSupport);
-    
-    % Zero eye movements
-    eyeMovementsNum = theOIsequence.maxEyeMovementsNumGivenIntegrationTime(theConeMosaic.integrationTime);
-    emPaths = zeros(instancesNum, eyeMovementsNum, 2);
-    
-    % Set rng seed if one was passed. Not clear we need to do this because
-    % all the randomness is in the @coneMosaic compute object, but it
-    % doesn't hurt to do so, if we ever choose a random number at this
-    % level.
-    if (~isempty(rngSeed))
-        oldSeed = rng(rngSeed);
-    end
-    
-    timeSamplesNum = eyeMovementsNum;
-    
-    % Compute responses for each type of noise flag requested
-    % Modified by Mengxin 2023/03/14: from computeForOISequence to compute
-    for idx = 1:length(noiseFlags)
-        if (contains(ieParamFormat(noiseFlags{idx}), 'none'))
-            % Compute the noise-free response
-            % To do so, first save the current mosaic noiseFlag
-            lastConeMosaicNoiseFlag = theConeMosaic.noiseFlag;
-            
-            % Set the coneMosaic.noiseFlag to 'none';
-            theConeMosaic.noiseFlag = 'none';
-            
-            % Compute noise-free response instances
-            [theNeuralResponses(noiseFlags{idx}), ~, ~, ~, temporalSupportSeconds] = theConeMosaic.compute(theOIsequence.frameAtIndex(1), ...
-                'nTrials', instancesNum, ...
-                'nTimePoints', timeSamplesNum ...
-            );
-        
-            % Restore the original noise flag
-            theConeMosaic.noiseFlag = lastConeMosaicNoiseFlag;
-            
-        elseif (~isempty(rngSeed))
-            % Compute noisy response instances with a specified random noise seed for repeatability
-            [~,theNeuralResponses(noiseFlags{idx}), ~, ~, temporalSupportSeconds] = theConeMosaic.compute(theOIsequence.frameAtIndex(1), ...
-                'nTrials', instancesNum, ...
-                'nTimePoints', timeSamplesNum, ...
-                'withFixationalEyeMovements', flase, ...
-                'seed', rngSeed ...        % random seed
-            );
-        
-        elseif (contains(ieParamFormat(noiseFlags{idx}), 'random'))
-            % Because computeForOISequence freezes noise, if we want
-            % unfrozen noise (which is the case if we are here), 
-            % we have to pass it a randomly chosen seed.
-            useSeed = randi(32000,1,1);
-            
-            % Compute noisy response instances
-            [~, theNeuralResponses(noiseFlags{idx}), ~, ~, temporalSupportSeconds] = theConeMosaic.compute(theOIsequence.frameAtIndex(1), ...
-                'nTrials', instancesNum, ...
-                'nTimePoints', timeSamplesNum, ...
-                'withFixationalEyeMovements', false, ...
-                'seed', useSeed ...        % random seed
-            );
+        % Check input arguments. If called with zero input arguments, just return the default params struct
+        if (nargin == 0)
+            dataOut = generateDefaultParams();
+            return;
         end
-    end
+        
+        % Parse the input arguments
+        p = inputParser;
+        p.addParameter('noiseFlags', {'random'});
+        p.addParameter('rngSeed',[],@(x) (isempty(x) | isnumeric(x)));
+        varargin = ieParamFormat(varargin);
+        p.parse(varargin{:});
+        
+        % Retrieve the response noiseFlag labels and validate them.
+        noiseFlags = p.Results.noiseFlags;
+        rngSeed = p.Results.rngSeed;
+        neuralEngineOBJ.validateNoiseFlags(noiseFlags);
+        
+        % For each noise flag we generate a corresponing neural response, and all 
+        % neural responses are stored in a dictionary indexed by the noiseFlag label.
+        % Setup theNeuralResponses dictionary, loading empty responses for now
+        theNeuralResponses = containers.Map();
+        for idx = 1:length(noiseFlags)
+            theNeuralResponses(noiseFlags{idx}) = [];
+        end
+        
+        if (isempty(neuralEngineOBJ.neuralPipeline))
+            % Generate the optics
+    %         theOptics = oiCreate(neuralResponseParamsStruct.opticsParams.type, neuralResponseParamsStruct.opticsParams.pupilDiameterMM);  
     
-    % Restore rng seed if we set it
-    if (~isempty(rngSeed))
-        rng(oldSeed);
-    end
+            % Generate the cone mosaic
+            % Modified by Mengxin 2023/03/14: switched from using coneMosaicHex
+            % to cMosaic; eccentricity added
+            theConeMosaic = cMosaic(...
+                'sizeDegs', neuralResponseParamsStruct.coneMosaicParams.sizeDegs, ... 
+                'eccentricityDegs', neuralResponseParamsStruct.coneMosaicParams.eccDegs, ...
+                'integrationTime', neuralResponseParamsStruct.coneMosaicParams.timeIntegrationSeconds ...
+                );
+            
+            % Generate optics appropriate for the mosaic's eccentricity
+            oiEnsemble = theConeMosaic.oiEnsembleGenerate(neuralResponseParamsStruct.coneMosaicParams.eccDegs, ...
+                'zernikeDataBase', 'Polans2015', ...
+                'subjectID', neuralResponseParamsStruct.opticsParams.PolansSubject, ...
+                'pupilDiameterMM', neuralResponseParamsStruct.opticsParams.pupilDiameterMM);
+            theOptics = oiEnsemble{1};
+            returnTheNeuralPipeline = true;
+        else
+            % Load the optics from the previously computed neural pipeline
+            theOptics = neuralEngineOBJ.neuralPipeline.optics;
+            % Load the cone mosaic from the previously computed neural pipeline
+            theConeMosaic = neuralEngineOBJ.neuralPipeline.coneMosaic;
+            returnTheNeuralPipeline =  false;
+        end
     
-    % Temporal support for the neural response
-%     temporalSupportSeconds = theConeMosaic.timeAxis; 
-    
-    % Assemble the dataOut struct
-    dataOut = struct(...
-        'neuralResponses', theNeuralResponses, ...
-    	'temporalSupport', temporalSupportSeconds);
-    if (returnTheNeuralPipeline)
-        dataOut.neuralPipeline.optics = theOptics;
-        dataOut.neuralPipeline.coneMosaic = theConeMosaic;
+        % Compute the sequence of optical images corresponding to the sequence of scenes
+        framesNum = numel(sceneSequence);
+        theListOfOpticalImages = cell(1, framesNum);
+        for frame = 1:framesNum
+            theListOfOpticalImages{frame} = oiCompute(sceneSequence{frame}, theOptics);
+        end
+        
+        % Generate an @oiSequence object containing the list of computed optical images
+        theOIsequence = oiArbitrarySequence(theListOfOpticalImages, sceneSequenceTemporalSupport);
+        
+        % Zero eye movements
+        eyeMovementsNum = theOIsequence.maxEyeMovementsNumGivenIntegrationTime(theConeMosaic.integrationTime);
+        emPaths = zeros(instancesNum, eyeMovementsNum, 2);
+        
+        % Set rng seed if one was passed. Not clear we need to do this because
+        % all the randomness is in the @coneMosaic compute object, but it
+        % doesn't hurt to do so, if we ever choose a random number at this
+        % level.
+        if (~isempty(rngSeed))
+            oldSeed = rng(rngSeed);
+        end
+        
+        timeSamplesNum = eyeMovementsNum;
+        
+        % Compute responses for each type of noise flag requested
+        % Modified by Mengxin 2023/03/14: from computeForOISequence to compute
+        for idx = 1:length(noiseFlags)
+            if (contains(ieParamFormat(noiseFlags{idx}), 'none'))
+                % Compute the noise-free response
+                % To do so, first save the current mosaic noiseFlag
+                lastConeMosaicNoiseFlag = theConeMosaic.noiseFlag;
+                
+                % Set the coneMosaic.noiseFlag to 'none';
+                theConeMosaic.noiseFlag = 'none';
+                
+                % Compute noise-free response instances
+                [theNeuralResponses(noiseFlags{idx}), ~, ~, ~, temporalSupportSeconds] = theConeMosaic.compute(theOIsequence.frameAtIndex(1), ...
+                    'nTrials', instancesNum, ...
+                    'nTimePoints', timeSamplesNum ...
+                );
+            
+                % Restore the original noise flag
+                theConeMosaic.noiseFlag = lastConeMosaicNoiseFlag;
+                
+            elseif (~isempty(rngSeed))
+                % Compute noisy response instances with a specified random noise seed for repeatability
+                [~,theNeuralResponses(noiseFlags{idx}), ~, ~, temporalSupportSeconds] = theConeMosaic.compute(theOIsequence.frameAtIndex(1), ...
+                    'nTrials', instancesNum, ...
+                    'nTimePoints', timeSamplesNum, ...
+                    'withFixationalEyeMovements', flase, ...
+                    'seed', rngSeed ...        % random seed
+                );
+            
+            elseif (contains(ieParamFormat(noiseFlags{idx}), 'random'))
+                % Because computeForOISequence freezes noise, if we want
+                % unfrozen noise (which is the case if we are here), 
+                % we have to pass it a randomly chosen seed.
+                useSeed = randi(32000,1,1);
+                
+                % Compute noisy response instances
+                [~, theNeuralResponses(noiseFlags{idx}), ~, ~, temporalSupportSeconds] = theConeMosaic.compute(theOIsequence.frameAtIndex(1), ...
+                    'nTrials', instancesNum, ...
+                    'nTimePoints', timeSamplesNum, ...
+                    'withFixationalEyeMovements', false, ...
+                    'seed', useSeed ...        % random seed
+                );
+            end
+        end
+        
+        % Restore rng seed if we set it
+        if (~isempty(rngSeed))
+            rng(oldSeed);
+        end
+        
+        % Temporal support for the neural response
+    %     temporalSupportSeconds = theConeMosaic.timeAxis; 
+        
+        % Assemble the dataOut struct
+        dataOut = struct(...
+            'neuralResponses', theNeuralResponses, ...
+    	    'temporalSupport', temporalSupportSeconds);
+        if (returnTheNeuralPipeline)
+            dataOut.neuralPipeline.optics = theOptics;
+            dataOut.neuralPipeline.coneMosaic = theConeMosaic;
+        end
+    else
+        error('Unrecognized command. Please type either ''yes'' or ''no'''); 
     end
 end
 
@@ -281,17 +296,4 @@ function p = generateDefaultParams()
             'timeIntegrationSeconds', 5/1000 ...
         ) ...
     );
-
-%     p = struct(...
-%         'opticsParams', struct(...
-%             'type', 'wvf human', ...
-%             'pupilDiameterMM', 3.0 ...
-%         ), ...
-%         'coneMosaicParams', struct(...
-%             'upsampleFactor', 5, ...
-%             'fovDegs', 0.3, ...
-%             'timeIntegrationSeconds', 5/1000 ...
-%         ) ...
-%     );
-
 end
