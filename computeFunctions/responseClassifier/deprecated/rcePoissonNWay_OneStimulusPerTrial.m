c9b78627d255c6d5d0ef9bfce8edc7962046d7ca
FangfangHong <fh862@nyu.edu>
2023-10-05 14:56:24 -0400

    edited computeThresholdTAFC.m

--- computeFunctions/responseClassifier/deprecated/rcePoissonNWay_OneStimulusPerTrial.m
+++ computeFunctions/responseClassifier/deprecated/rcePoissonNWay_OneStimulusPerTrial.m
@@ -1,164 +1,80 @@
 function dataOut = rcePoissonNWay_OneStimulusPerTrial(obj, operationMode, classifierParamsStruct, theResponses, whichAlternatives)
-% Compute function for ideal signal-known Poisson noise classifier for N-way forced choice.
+% Compute function for ideal signal-known Poisson noise classifier for 
+% N-way forced choice. This function has been deprecated because there is a
+% more general function rcePoisson.m that is applicable for both N-way
+% forced choice and TAFC tasks.
 %
-% Syntax:
-%     dataOut = rcePoissonNWay_OneStimulusPerTrial(obj, operationMode,classifierParamsStruct, theReponses, whichAlternatives)
-%
-% Description:
-%    Compute function to be used as a computeFunctionHandle for a
-%    @responseClassifierEngine object.
-%
-%    When called with no arguments, this returns a default parameters
-%    struct for itself. Currently, there are no parameters used for this
-%    routine, so a struct with an unintersting field is returned just to
-%    keep the calling machinery happy.
-%
-%    When called from a parent @responseClassifierEngine object with operationMode set to 'predict', it
-%    sets up the Poission log-likehood ratio (LL) for the decision, using
-%    the mean of the passed N responses as a template.  Pass
-%    noise free responses to get the signal known exactly version.
-%
-%    When this is called from a parent @responseClassifierEngine object
-%    with operationMode set to 'predict', it makes correct/incorrect
-%    predictions for the passed instances. The predictions are made using a
-%    maximum likelihood classifier for N-way forced choice and Poisson noise.
-%
-%    This function models an N-way forced choice task with one stimulus
-%    presented per trial, so that each trial is has responses to be one of the
-%    N possible alternatives.
-%
-%    Typically, training will be with one noise free instances of each of
-%    the N alternatives, while testing will be on noisy instances.  For
-%    training, however, the means of the passed instances are taken as the
-%    noise free template, so that you can in fact pass multiple instances
-%    per alternative and they can be noisy.
-%
-% Inputs:
-%    obj                      - The calling @responseClassifierEngine.
-%    operationMode            - String, may be either 'train' or
-%                               'predict'. When set to 'train', the
-%                               function stores into the passed object the
-%                               information it needs to predict, using the
-%                               passed instances.  When set to 'predict'
-%                               the function predicts correct/incorrect on
-%                               the passed instances.
-%    classifierParamsStruct   - Paramters struct passed by the object.  Can
-%                               be used to control details of how the classifier
-%                               is set up. Currently this function does not
-%                               use any parameters.
-%               In 'train' mode:    
-%                    theResponses   - an N-dimensional cell array, with each entry
-%                                     a [mInstances x nDims x tTimePoints] matrix
-%                                     of responses for one of the N alternatives.
-%                    whichAlternatives - Not used. 
-%
-%               In 'predict' mode:  
-%                    theResponses   - an [mTrials x nDims x nTimePoints]
-%                                     matrix of responses for each trial.
-%                    whichAlternatives - Vector of dimension mTrials whose
-%                                     entries are integers in the range [1,N] and which
-%                                     specify the alternative presented on each trial.
-%
-% Outputs:
-%    dataOut                  - If function called with no input arguments,
-%                               this is the default parameter structure for
-%                               this funciton.
-%                              
-%                               If function is called from a
-%                               @responseClassifierEngine object, what this
-%                               contains depends on whether operationMode
-%                               is 'train' or 'predict'.
-% 
-%                               For 'train', the struct has two fields as
-%                               required by responseClassifierEngine
-%                               objects.
-%                                 .trainedClassifier: Set to empty matrix
-%                                 .preProcessingConstants: Structure with
-%                                   templates to be used by classifier to
-%                                   predict.
-%             
-%                              For 'predict', the struct has two fields as
-%                               required by responseClassifierEngine
-%                               objects.
-%                                 .trialPredictions : Vector with trial-by-trial
-%                                                     correct/incorrect
-%                                                     predictions. 1 means
-%                                                     correct, 0 means incorrect.
-%                                 .pCorrect         : Probability correct, which is
-%                                                     just the mean of the trialPredictions
-%                                                     field.
-%
-% Optional key/value pairs:
-%   None.
-%
-% See also: t_thresholdEngine, t_responseClassifer, rcePoissonTAFC,
-%           PoissonDecisionLogLikelihood,
-%           PoissonIdealObserverNAlternativeFC.
-%            
-
 % History:
 %   12/03/21  dhb  Started on this.
+%   05/10/23  fh   Edited the code to direct people to the more general
+%                   function rcePoisson.m and moved this to the deprecated
+%                   folder
 
 % For consistency with the interface
 if (nargin == 0)
-    dataOut = struct('Classifier', 'Poisson N-way Forced Choice Ideal Observer');
+    warning(['This function has been deprecated. Consider using a more ',...
+        'general function rcePoisson.m. ']);
+    dataOut = struct('Classifier', 'Poisson Ideal Observer');
     return;
 end
 
-% Check operation mode
-if (~strcmp(operationMode,'train') && ~strcmp(operationMode,'predict'))
-    error('Unknown operation mode passed.  Must be ''train'' or ''predict''');
-end
-    
-if (strcmp(operationMode, 'train'))  
-    % No noise response template for each alternative stimulus 
-    for ii = 1:length(theResponses)
-        theTemplates{ii} = mean(theResponses{ii}(:, :), 1);
-    end
-    dataOut.trainedClassifier = [];
-    dataOut.preProcessingConstants.theTemplates = theTemplates;
-    
-    return;
-end
+dataOut = rcePoisson(obj, operationMode, classifierParamsStruct,...
+    theResponses, whichAlternatives);
 
-if (strcmp(operationMode, 'predict'))
-    % Get templates we tucked away at training time.
-    theTemplates = obj.preProcessingConstants.theTemplates;
-    
-    % Make sure number of instances matches across passed responses.
-    nTrials = size(theResponses, 1);
-    assert(nTrials == length(whichAlternatives));
-    
-    % Compute response finding which of the possible alternatives has the
-    % highest likelihood.
-    response = zeros(1, nTrials);
-    nAlternatives = length(theTemplates);
-    trialDecisionLikelihoods = zeros(nTrials,nAlternatives);
-    for tt = 1:nTrials
-        % Find the decision likelihood
-        for aa = 1:nAlternatives
-            trialDecisionLikelihoods(tt,aa) = PoissonDecisionLogLikelihoood(theResponses(tt, :), theTemplates{aa});
-        end
-
-        % Pick the winner
-        [~,whichAlternativeList] = max(trialDecisionLikelihoods(tt,:));
-        whichAlternative = whichAlternativeList(1);
-
-        % See if it was correct
-        if (whichAlternative == whichAlternatives(tt))
-            response(tt) = 1;
-        else
-            response(tt) = 0;
-        end
-    end
-    
-    % Set up return
-    dataOut.trialPredictions = response;
-    dataOut.pCorrect = mean(response);
-    
-    return;
-end
-
-end
-
-
+% % Check operation mode
+% if (~strcmp(operationMode,'train') && ~strcmp(operationMode,'predict'))
+%     error('Unknown operation mode passed.  Must be ''train'' or ''predict''');
+% end
+% 
+% if (strcmp(operationMode, 'train'))  
+%     % No noise response template for each alternative stimulus 
+%     for ii = 1:length(theResponses)
+%         theTemplates{ii} = mean(theResponses{ii}(:, :), 1);
+%     end
+%     dataOut.trainedClassifier = [];
+%     dataOut.preProcessingConstants.theTemplates = theTemplates;
+% 
+%     return;
+% end
+% 
+% if (strcmp(operationMode, 'predict'))
+%     % Get templates we tucked away at training time.
+%     theTemplates = obj.preProcessingConstants.theTemplates;
+% 
+%     % Make sure number of instances matches across passed responses.
+%     nTrials = size(theResponses, 1);
+%     assert(nTrials == length(whichAlternatives));
+% 
+%     % Compute response finding which of the possible alternatives has the
+%     % highest likelihood.
+%     response = zeros(1, nTrials);
+%     nAlternatives = length(theTemplates);
+%     trialDecisionLikelihoods = zeros(nTrials,nAlternatives);
+%     for tt = 1:nTrials
+%         % Find the decision likelihood
+%         for aa = 1:nAlternatives
+%             trialDecisionLikelihoods(tt,aa) = PoissonDecisionLogLikelihoood(theResponses(tt, :), theTemplates{aa});
+%         end
+% 
+%         % Pick the winner
+%         [~,whichAlternativeList] = max(trialDecisionLikelihoods(tt,:));
+%         whichAlternative = whichAlternativeList(1);
+% 
+%         % See if it was correct
+%         if (whichAlternative == whichAlternatives(tt))
+%             response(tt) = 1;
+%         else
+%             response(tt) = 0;
+%         end
+%     end
+% 
+%     % Set up return
+%     dataOut.trialPredictions = response;
+%     dataOut.pCorrect = mean(response);
+% 
+%     return;
+% end
+% 
+% end
+% 
+% 
--- computeFunctions/responseClassifier/deprecated/rcePoissonTAFC.m
+++ computeFunctions/responseClassifier/deprecated/rcePoissonTAFC.m
@@ -1,96 +1,22 @@
 function dataOut = rcePoissonTAFC(obj, operationMode, classifierParamsStruct, nullResponses, testResponses)
-% Compute function for ideal signal-known Poisson noise classifier for TAFC
+% Compute function for ideal signal-known Poisson noise classifier for
+% TAFC. This function has been deprecated because there is a more general 
+% function rcePoisson.m that is applicable for both N-way forced choice 
+% and TAFC tasks.
 %
-% Syntax:
-%     dataOut = rcePoissonTAFC(obj, operationMode, classifierParamsStruct, nullResponses, testResponses)
-%
-% Description:
-%    Compute function to be used as a computeFunctionHandle for a
-%    @responseClassifierEngine object.
-%
-%    When called with no arguments, this returns a default parameters
-%    struct for itself. Currently, there are no parameters used for this
-%    routine, so a struct with an unintersting field is returned just to
-%    keep the calling machinery happy.
-%
-%    When called from a parent @responseClassifierEngine object with operationMode set to 'predict', it
-%    sets up the Poission log-likehood ratio (LL) of test stimulus, using
-%    the mean of the passed null and test responses as a template.  Pass
-%    noise free responses to get the signal known exactly version.
-%
-%    When this is called from a parent @responseClassifierEngine object
-%    with operationMode set to 'predict', it makes correct/incorrect
-%    predictions for the passed instances. The predictions are made using a
-%    maximum likelihood classifier for TAFC and Poisson noise.
-%
-%    This function models a TAFC task, so that each trial is considered to
-%    be either null/test across the two alternatives, or test/null.
-%
-%    Typically, training will be with one noise free instances of the null
-%    and test responses, while testing will be on noisy instances.  For
-%    training, however, the means of the passed null and test instances are
-%    taken as the noise free template, so that you can in fact pass
-%    multiple instances and they can be noisy.
-%
-% Inputs:
-%    obj                      - The calling @responseClassifierEngine.
-%    operationMode            - String, may be either 'train' or
-%                               'predict'. When set to 'train', the
-%                               function stores into the passed object the
-%                               information it needs to predict, using the
-%                               passed instances.  When set to 'predict'
-%                               the function predicts correct/incorrect on
-%                               the passed instances.
-%    classifierParamsStruct   - Paramters struct passed by the object.  Can
-%                               be used to control details of how the classifier
-%                               is set up. Currently this function does not
-%                               use any parameters.
-%    nullResponses            - an [mInstances x nDims x tTimePoints] matrix of responses to the null stimulus
-%    testResponses            - an [mInstances x nDims x tTimePoints] matrix of responses to the test stimulus
-%
-% Outputs:
-%    dataOut                  - If function called with no input arguments,
-%                               this is the default parameter structure for
-%                               this funciton.
-%                              
-%                               If function is called from a @responseClassifierEngine
-%                               object, what this contains depends on
-%                               whether operationMode is 'train' or
-%                               'predict'.
-% 
-%                               For 'train', the struct has two fields as
-%                               required by responseClassifierEngine
-%                               objects.
-%                                 .trainedClassifier: Set to empty matrix
-%                                 .preProcessingConstants: Structure with
-%                                   templates to be used by classifier to
-%                                   predict.
-%             
-%                              For 'predict', the struct has two fields as
-%                               required by responseClassifierEngine
-%                               objects.
-%                                 .trialPredictions : Vector with trial-by-trial
-%                                                     correct/incorrect
-%                                                     predictions. 1 means
-%                                                     correct, 0 means incorrect.
-%                                 .pCorrect         : Probability correct, which is
-%                                                     just the mean of the trialPredictions
-%                                                     field.
-%
-% Optional key/value pairs:
-%   None.
-%
-% See also: t_thresholdEngine, t_responseClassifer, rcePoissonNWay,
-%           PoissonDecisionLogLikelihood
-
 % History:
 %   10/17/20  dhb  Lots of work on comments.
 %   12/03/21  dhb  Pull out Poisson log likelihood as function into
 %                  isetbio.
+%   05/10/23  fh   Edited the code to direct people to the more general
+%                   function rcePoisson.m and moved this to the deprecated
+%                   folder
 
 % For consistency with the interface
 if (nargin == 0)
-    dataOut = struct('Classifier', 'Poisson 2AFC Ideal Observer');
+    warning(['This function has been deprecated. Consider using a more ',...
+        'general function rcePoisson.m. ']);
+    dataOut = struct('Classifier', 'Poisson Ideal Observer');
     return;
 end
 
@@ -98,53 +24,81 @@ end
 if (~strcmp(operationMode,'train') && ~strcmp(operationMode,'predict'))
     error('Unknown operation mode passed.  Must be ''train'' or ''predict''');
 end
-    
-if (strcmp(operationMode, 'train'))  
-    % We simulate the observer with a "detection" protocol
-    % No noise response template for test/null stimulus 
-    nullTemplate = mean(nullResponses(:, :), 1);
-    testTemplate = mean(testResponses(:, :), 1);
-    dataOut.trainedClassifier = [];
-    dataOut.preProcessingConstants = struct('nullTemplate', nullTemplate, 'testTemplate', testTemplate);
-    
-    return;
+
+%reorganize the null and the test responses
+if strcmp(operationMode, 'train')
+    %concatenate them along the 3rd dimension (cones)
+    cat1 = cat(3, testResponses, nullResponses);
+    cat2 = cat(3, nullResponses, testResponses);
+    %nicely put the concatenated cells back to the container
+    theResponses = containers.Map(trainNoiseFlag, {cat1, cat2});
+    dataOut = rcePoisson(obj, operationMode, classifierParamsStruct,...
+        theResponses,[]);
+else
+    %combine the 2nd (time) and the 3rd dimensions (cones)
+    outSampleTestStimResponses = testResponses(:,:);
+    outSampleNullStimResponses = nullResponses(:,:);
+    %concatenate them together along the 2nd dimension (cones)
+    cat_resp = cat(2, outSampleTestStimResponses, outSampleNullStimResponses);
+    %nicely put them back to the container
+    theResponses = containers.Map(testNoiseFlag, cat_resp);
+    %define the label for correct responses
+    whichAlternatives = ones(size(theResponses,1),1);
+    dataOut = rcePoisson(obj, operationMode, classifierParamsStruct,...
+        theResponses,whichAlternatives);
 end
 
-if (strcmp(operationMode, 'predict'))
-    % Get template we tucked away at training time.
-    nullTemplate = obj.preProcessingConstants.nullTemplate;
-    testTemplate = obj.preProcessingConstants.testTemplate;
-    
-    % Make sure number of null and test instances matches.
-    nTrials = size(nullResponses, 1);
-    assert(nTrials == size(testResponses, 1));
-    
-    % Compute response {0, 1} with log likelihood ratio.  Assume without
-    % loss of generality that alternative order on each trial is null/test,
-    % compute likelihood of each order, and call it correct if likelihood
-    % for null/test order is higher.  Because each entry of the response
-    % vectors is an indendent Poisson observation, we can just sum the log
-    % likelihood of the null and test components of the TAFC response.
-    response = zeros(1, nTrials);
-    for idx = 1:nTrials
-        llhdCr = PoissonDecisionLogLikelihoood(nullResponses(idx, :), nullTemplate) + PoissonDecisionLogLikelihoood(testResponses(idx, :), testTemplate);
-        llhdIc = PoissonDecisionLogLikelihoood(nullResponses(idx, :), testTemplate) + PoissonDecisionLogLikelihoood(testResponses(idx, :), nullTemplate);
-        
-        % For likelihood ratio extremely close to 1, do a coin flip
-        threshold = 1e-10;
-        if (abs(llhdCr - llhdIc) <= threshold)
-            response(idx) = (rand() > 0.5);
-        else
-            response(idx) = ((llhdCr - llhdIc) > 0);
-        end
-    end
-    
-    % Set up return
-    dataOut.trialPredictions = response;
-    dataOut.pCorrect = mean(response);
-    
-    return;
-end
-
-end
-
+% % Check operation mode
+% if (~strcmp(operationMode,'train') && ~strcmp(operationMode,'predict'))
+%     error('Unknown operation mode passed.  Must be ''train'' or ''predict''');
+% end
+% 
+% if (strcmp(operationMode, 'train'))  
+%     % We simulate the observer with a "detection" protocol
+%     % No noise response template for test/null stimulus 
+%     nullTemplate = mean(nullResponses(:, :), 1);
+%     testTemplate = mean(testResponses(:, :), 1);
+%     dataOut.trainedClassifier = [];
+%     dataOut.preProcessingConstants = struct('nullTemplate', nullTemplate, 'testTemplate', testTemplate);
+% 
+%     return;
+% end
+% 
+% if (strcmp(operationMode, 'predict'))
+%     % Get template we tucked away at training time.
+%     nullTemplate = obj.preProcessingConstants.nullTemplate;
+%     testTemplate = obj.preProcessingConstants.testTemplate;
+% 
+%     % Make sure number of null and test instances matches.
+%     nTrials = size(nullResponses, 1);
+%     assert(nTrials == size(testResponses, 1));
+% 
+%     % Compute response {0, 1} with log likelihood ratio.  Assume without
+%     % loss of generality that alternative order on each trial is null/test,
+%     % compute likelihood of each order, and call it correct if likelihood
+%     % for null/test order is higher.  Because each entry of the response
+%     % vectors is an indendent Poisson observation, we can just sum the log
+%     % likelihood of the null and test components of the TAFC response.
+%     response = zeros(1, nTrials);
+%     for idx = 1:nTrials
+%         llhdCr = PoissonDecisionLogLikelihoood(nullResponses(idx, :), nullTemplate) + PoissonDecisionLogLikelihoood(testResponses(idx, :), testTemplate);
+%         llhdIc = PoissonDecisionLogLikelihoood(nullResponses(idx, :), testTemplate) + PoissonDecisionLogLikelihoood(testResponses(idx, :), nullTemplate);
+% 
+%         % For likelihood ratio extremely close to 1, do a coin flip
+%         threshold = 1e-10;
+%         if (abs(llhdCr - llhdIc) <= threshold)
+%             response(idx) = (rand() > 0.5);
+%         else
+%             response(idx) = ((llhdCr - llhdIc) > 0);
+%         end
+%     end
+% 
+%     % Set up return
+%     dataOut.trialPredictions = response;
+%     dataOut.pCorrect = mean(response);
+% 
+%     return;
+% end
+% 
+% end
+% 
