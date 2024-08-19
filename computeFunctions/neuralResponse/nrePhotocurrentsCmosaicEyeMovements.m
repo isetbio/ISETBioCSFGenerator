ef928c2d709f590356d5af0158ed8c83cacd3936
David Brainard <brainard@psych.upenn.edu>
2024-08-19 09:49:34 -0400

    Pull photocurrent functions out separately

--- computeFunctions/neuralResponse/nrePhotocurrentsCmosaicEyeMovements.m
+++ computeFunctions/neuralResponse/nrePhotocurrentsCmosaicEyeMovements.m
@@ -289,51 +289,7 @@ function dataOut = nrePhotocurrentsCmosaicEyeMovements(...
     end
 end
 
-function impulseResponse = computePhotocurrentImpulseResponse(timeAxis)
 
-    % Temporary solution until we program photocurrent reponse computation
-    % in @cMosaic
-    load(fullfile(csfGeneratorRootPath,'osLinearFilters25CDM2.mat'), 'osLinearFilters', 'dt');
-    t = (1:size(osLinearFilters,1))*dt;
-    
-    impulseResponse = 2*interp1(t, squeeze(osLinearFilters(:,1)), timeAxis);
-end
-
-
-function photocurrentResponses = computePhotocurrent(coneExcitationResponses, temporalSupportSeconds, noiseFlag, responseSecondsKept)
-    % Compute photocurrent responses from the cone excitation responses
-    photocurrentResponses = 0*coneExcitationResponses;
-
-    impulseResponse = computePhotocurrentImpulseResponse(temporalSupportSeconds);
-    dt = temporalSupportSeconds(2)-temporalSupportSeconds(1);
-    
-    for iTrial = 1:size(coneExcitationResponses,1)
-        parfor iCone = 1:size(coneExcitationResponses,3)
-            coneExcitation = squeeze(coneExcitationResponses(iTrial,:,iCone));
-            pCurrent = conv(coneExcitation, impulseResponse, 'same');
-            if (~strcmp(noiseFlag, 'none'))
-                photocurrentResponses(iTrial,:,iCone) = osAddNoise(pCurrent, 'sampTime', dt);
-            else
-                photocurrentResponses(iTrial,:,iCone) = pCurrent;
-            end
-            
-%             figure(33);
-%             subplot(2,1,1);
-%             plot(temporalSupportSeconds, coneExcitation);
-%             subplot(2,1,2);
-%             plot(temporalSupportSeconds, squeeze(photocurrentResponses(iTrial,:,iCone)));
-%             pause
-        end
-    end
-    
-    % Keep the last specified msec of the photocurrent response
-    dt = temporalSupportSeconds(2)-temporalSupportSeconds(1);
-    idx = find(temporalSupportSeconds > temporalSupportSeconds(end) +dt - responseSecondsKept);
-    fprintf('Computed photocurrent response over a time duration of %d msec\n', numel(temporalSupportSeconds)*dt*1000);
-    fprintf('Keeping the last %d mseconds of the response to allow for pCurrent stabilization.\n',  responseSecondsKept*1000);
-    photocurrentResponses = photocurrentResponses(:,idx,:);
-
-end
 
 function p = generateDefaultParams()
     % Default params for this compute function
