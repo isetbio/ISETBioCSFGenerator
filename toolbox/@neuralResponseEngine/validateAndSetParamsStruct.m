ca5d1ec9909571f933b5c30e2cb77e37a10092ba
David Brainard <brainard@psych.upenn.edu>
2024-12-18 12:52:15 -0500

    The beginning of the end

--- toolbox/@neuralResponseEngine/validateAndSetParamsStruct.m
+++ toolbox/@neuralResponseEngine/validateAndSetParamsStruct.m
@@ -1,37 +1,30 @@
-function validateAndSetParamsStruct(obj, paramsStruct)
+function validateAndSetParamsStruct(obj, noiseFreeComputeParams,noisyInstancesComputeParams)
     
     % Accept an empty params struct
-    if (isempty(paramsStruct))
-        % Set the neuralParams to the default params struct, which is returned
+    if (isempty(noiseFreeComputeParams))
+        % Set the noiseFreeNeuralParams to the default params struct, which is returned
         % by the neuralComputeFunction when it is called with no input arguments
-        obj.neuralParams = obj.neuralComputeFunction();
-        return;
+        noiseFreeComputeParams = obj.noiseFreeComputeFunction();
     end
     
     % Assert that the input is a struct
-    assert(isstruct(paramsStruct), ...
+    assert(isstruct(noiseFreeComputeParams), ...
         'Expected a struct during neuralResponseEngine instantiation');
     
-    % DHB: This type checking too restrictive as our view of what is
-    % possible with this general compute structure expands.  Commented out.
-    %
-%    % Obligatory sub-truct: 'opticsParams'
-%    % Assert that we have a sub-struct named 'opticsParams'
-%    assert((isfield(paramsStruct, 'opticsParams'))&&(isstruct(paramsStruct.opticsParams)), ...
-%        'Expected an ''opticsParams'' substruct during neuralResponseEngine instantiation.');
-%    
-%     % Obligatory sub-struct: 'coneMosaicParams'
-%     % Assert that we have a sub-struct named 'coneMosaicParams'
-%     assert((isfield(paramsStruct, 'coneMosaicParams'))&&(isstruct(paramsStruct.coneMosaicParams)), ...
-%         'Expected a ''coneMosaicParams'' substruct during neuralResponseEngine instantiation.');
-%     
-%     % Optional sub-struct: 'rgcMosaicParams'
-%     % Assert that we have a sub-struct named 'coneMosaicParams'
-%     if (isfield(paramsStruct, 'rgcMosaicParams'))
-%         assert((isstruct(paramsStruct.rgcMosaicParams)), ...
-%             'Optional ''rgcMosaicParams''passed during neuralResponseEngine instantiation is not a struct.');
-%    end
+    % Set the neural params
+    obj.noiseFreeComputeParams = noiseFreeComputeParams;
+
+        % Accept an empty params struct
+    if (isempty(noisyInstancesComputeParams))
+        % Set the noisyInstancesNeuralParams to the default params struct, which is returned
+        % by the neuralComputeFunction when it is called with no input arguments
+        noisyInstancesComputeParams = obj.noisyInstancesComputeFunction();
+    end
+    
+    % Assert that the input is a struct
+    assert(isstruct(noisyInstancesComputeParams), ...
+        'Expected a struct during neuralResponseEngine instantiation');
     
     % Set the neural params
-    obj.neuralParams = paramsStruct;
+    obj.noisyInstancesComputeParams = noisyInstancesComputeParams;
 end
